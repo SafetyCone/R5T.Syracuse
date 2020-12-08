1af84c4056a42371c2bbdf05eb10314e5bff7385
David Coats <david@getsafetycone.com>
2020-12-07 22:10:57 -0800

    Added IService interface abstraction type to allow marking other types as services

---
+++ source/R5T.Syracuse/Code/Interfaces/IService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace R5T.Syracuse
+{
+    /// <summary>
+    /// A marker interface indicating that a type is a service.
+    /// </summary>
+    public interface IService
+    {
+    }
+}
